b34b999ee49238d04ba04814f5351b288908b12b
Connor Howington <connor.howington@gmail.com>
2026-03-08 15:32:49 -0500

    Remove About page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -14,11 +14,6 @@ const router = createRouter({
       name: 'login',
       component: () => import('../views/LoginView.vue'),
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
   ],
 })
 
